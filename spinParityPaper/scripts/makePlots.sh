d5c9856351114ec84608c67e14edb1817b5dec1e
whitbeck <>
2012-08-16 19:51:15 +0000

    *** empty log message ***

--- spinParityPaper/scripts/makePlots.sh
+++ spinParityPaper/scripts/makePlots.sh
@@ -58,14 +58,13 @@ if [ $1 == "toys" ]
     fi
 	while [ $[$n] -ge 0 ] ; do 
 	  
-	  qsub submitPBS_runToys.pbs -v "var1=0, var2=10."
-	  qsub submitPBS_runToys.pbs -v "var1=1, var2=10."
-	  #qsub submitPBS_runToys.pbs -v "var1=2, var2=22."
-	  #qsub submitPBS_runToys.pbs -v "var1=3, var2=22."
-	  #qsub submitPBS_runToys.pbs -v "var1=4, var2=10."
-	  #qsub submitPBS_runToys.pbs -v "var1=5, var2=10."
-	  #qsub submitPBS_runToys.pbs -v "var1=6, var2=22."
-
+	  qsub submitPBS_runToys.pbs -v "var1=0, var2=22."
+	  qsub submitPBS_runToys.pbs -v "var1=1, var2=22."
+	  qsub submitPBS_runToys.pbs -v "var1=2, var2=22."
+	  qsub submitPBS_runToys.pbs -v "var1=3, var2=22."
+	  qsub submitPBS_runToys.pbs -v "var1=4, var2=10."
+	  qsub submitPBS_runToys.pbs -v "var1=5, var2=10."
+	  qsub submitPBS_runToys.pbs -v "var1=6, var2=22."
 
 	  n=$[$n-1]
 
