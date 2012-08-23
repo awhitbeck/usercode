33e3a42bf94aeedeebdb9204db88b5e36f635b44
whitbeck <>
2012-08-23 14:26:28 +0000

    fixed absolute path ... now uses CMSSW_BASE

--- lowMassAnalysis1D/makeDataCards/datacards_TEMPLATE/myCombineCards.sh
+++ lowMassAnalysis1D/makeDataCards/datacards_TEMPLATE/myCombineCards.sh
@@ -6,18 +6,18 @@ cd $DIR
 
 #combine all cards:
 
-~/scratch0/HiggsCombination/CMSSW_4_4_0/src/HiggsAnalysis/CombinedLimit/scripts/combineCards.py CMS_hzz2l2q_ee0b=hzz2l2q_ee0b_7TeV.txt CMS_hzz2l2q_ee1b=hzz2l2q_ee1b_7TeV.txt CMS_hzz2l2q_ee2b=hzz2l2q_ee2b_7TeV.txt CMS_hzz2l2q_mm0b=hzz2l2q_mm0b_7TeV.txt CMS_hzz2l2q_mm1b=hzz2l2q_mm1b_7TeV.txt CMS_hzz2l2q_mm2b=hzz2l2q_mm2b_7TeV.txt > CMS_hzz2l2q_${DIR}_6channels.txt
+combineCards.py CMS_hzz2l2q_ee0b=hzz2l2q_ee0b_7TeV.txt CMS_hzz2l2q_ee1b=hzz2l2q_ee1b_7TeV.txt CMS_hzz2l2q_ee2b=hzz2l2q_ee2b_7TeV.txt CMS_hzz2l2q_mm0b=hzz2l2q_mm0b_7TeV.txt CMS_hzz2l2q_mm1b=hzz2l2q_mm1b_7TeV.txt CMS_hzz2l2q_mm2b=hzz2l2q_mm2b_7TeV.txt > CMS_hzz2l2q_${DIR}_6channels.txt
  
 #combine 0btag
  
- ~/scratch0/HiggsCombination/CMSSW_4_4_0/src/HiggsAnalysis/CombinedLimit/scripts/combineCards.py CMS_hzz2l2q_ee0b=hzz2l2q_ee0b_7TeV.txt CMS_hzz2l2q_mm0b=hzz2l2q_mm0b_7TeV.txt > CMS_hzz2l2q_${DIR}_0btag.txt
+combineCards.py CMS_hzz2l2q_ee0b=hzz2l2q_ee0b_7TeV.txt CMS_hzz2l2q_mm0b=hzz2l2q_mm0b_7TeV.txt > CMS_hzz2l2q_${DIR}_0btag.txt
  
 #combine 1btag
  
- ~/scratch0/HiggsCombination/CMSSW_4_4_0/src/HiggsAnalysis/CombinedLimit/scripts/combineCards.py CMS_hzz2l2q_ee1b=hzz2l2q_ee1b_7TeV.txt CMS_hzz2l2q_mm1b=hzz2l2q_mm1b_7TeV.txt > CMS_hzz2l2q_${DIR}_1btag.txt
+combineCards.py CMS_hzz2l2q_ee1b=hzz2l2q_ee1b_7TeV.txt CMS_hzz2l2q_mm1b=hzz2l2q_mm1b_7TeV.txt > CMS_hzz2l2q_${DIR}_1btag.txt
 
 #combine 2btag
 
- ~/scratch0/HiggsCombination/CMSSW_4_4_0//src/HiggsAnalysis/CombinedLimit/scripts/combineCards.py CMS_hzz2l2q_ee2b=hzz2l2q_ee2b_7TeV.txt CMS_hzz2l2q_mm2b=hzz2l2q_mm2b_7TeV.txt > CMS_hzz2l2q_${DIR}_2btag.txt
+combineCards.py CMS_hzz2l2q_ee2b=hzz2l2q_ee2b_7TeV.txt CMS_hzz2l2q_mm2b=hzz2l2q_mm2b_7TeV.txt > CMS_hzz2l2q_${DIR}_2btag.txt
 
 cd -
