04fb59c4b87f68a81907e820de52986eb9cfd9b6
yygao <>
2013-06-20 21:46:54 +0000

    add the script to submit toys for ilc fits

---
+++ snowmass2013/test/submitilctoy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Edit the template file submitPBSJobTemplate.pbs 
+# make sure you edit 
+#    1. output directory
+#    2. the macro (.C) file with the correct argument
+# use showq -u username to check the status of the job
+
+export NJOBS=$1
+export NTOYSPERJOB=$2
+export ISPURETOY=$3
+
+if [ ! $# -eq 3 ]; then
+        echo "USAGE: ./submitilctoy.sh NJOBS NTOYSPERJOB ISPURETOY
+        NJOBS - number of jobs
+        NTOYSPERJOB - number of toys per job 
+        ISPURETOY - choose true for puretoy and false for embedded "
+    exit 1
+fi
+
+i=0; 
+
+while [ $i -lt $NJOBS ] ; 
+  do qsub submitPBSJobTemplate.pbs -v "pureToys=$ISPURETOY, ntoysperjob=$NTOYSPERJOB, seed_index=$i"
+  i=$[i+1];  
+done
