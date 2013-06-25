d167d2192b0ad855de0b5a6262bd602a5c386a6c
whitbeck <>
2013-06-25 17:29:02 +0000

    correcting calculation of eta and pt, now in sync with code from http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/UserCode/YGao/HZZ4lME/runME_HZZ4l_JHUgenSamples.C?view=log

--- snowmass2013/test/calculatePtEta.C
+++ snowmass2013/test/calculatePtEta.C
@@ -1,17 +1,23 @@
+#include <iostream>
+#include <vector>
+#include "TLorentzVector.h"
+#include "TChain.h"
+#include "TLorentzRotation.h"
+#include "TH1F.h"
+#include "TLegend.h"
 
-
-
+vector<TLorentzVector> Calculate4Momentum(float Mx,float M1,float M2,float theta,float theta1,float theta2,float Phi1,float Phi);
 
 void calculatePtEta(){
 
-  gROOT->ProcessLine(".L ~/tdrstyle.C");
-  setTDRStyle();
+  //gROOT->ProcessLine(".L ~/tdrstyle.C");
+  //setTDRStyle();
   //gStyle->SetOptStat(1);
 
   TChain* t = new TChain("SelectedTree");
   t->Add("/export/d1/scratch/snowmass2013/ilcsamples/signal/spin0/Events_20130618/unweighted_unpol_g1_1M_false.root");
 
-  float costheta1,costheta2,phi;
+  float costhetastar=0,costheta1,costheta2,phi,phi1=0;
   float l1pt,l2pt;
   float l1eta,l2eta;
   float ptlplus,ptlminus;
@@ -22,9 +28,10 @@ void calculatePtEta(){
   float mH = 125.;
   float mZ = 91.18;
 
+  //t->SetBranchAddress("costhetastar",&costhetastar);
   t->SetBranchAddress("costheta1",&costheta1);
   t->SetBranchAddress("costheta2",&costheta2);
-  t->SetBranchAddress("phi",&phi);
+  //t->SetBranchAddress("phistar1",&phi1);
   t->SetBranchAddress("phi",&phi);
   t->SetBranchAddress("ptlplus",&ptlplus);
   t->SetBranchAddress("ptlminus",&ptlminus);
@@ -33,98 +40,24 @@ void calculatePtEta(){
   //t->SetBranchAddress("ZMass",&mZ);
   //t->SetBranchAddress("HMass",&mH);
  
-  //TH1F* difflep1 = new TH1F("difflep1",";#Delta p_{T}/p_{T};",100,-1,1);
-  TH1F* difflep1 = new TH1F("difflep1",";#Delta #eta/#eta;",100,-1,1);
-  //TH1F* difflep2 = new TH1F("difflep2",";#Delta p_{T}/p_{T};",100,-1,1);
-  TH1F* difflep2 = new TH1F("difflep2",";#Delta #eta/#eta;",100,-1,1);
+  TH1F* difflep1 = new TH1F("difflep1",";#Delta p_{T}/p_{T};",100,-1,1);
+  //TH1F* difflep1 = new TH1F("difflep1",";#Delta #eta/#eta;",100,-1,1);
+  TH1F* difflep2 = new TH1F("difflep2",";#Delta p_{T}/p_{T};",100,-1,1);
+  //TH1F* difflep2 = new TH1F("difflep2",";#Delta #eta/#eta;",100,-1,1);
   difflep1->SetLineWidth(2);
   difflep1->SetLineColor(2);
 
   for(int i = 0 ; i<t->GetEntries(); i++){
    
     t->GetEntry(i);
- 
-    float pZsquare = (s*s+mZ*mZ-mH*mH)*(s*s+mZ*mZ-mH*mH)/(2*s)/(2*s) - mZ*mZ;
-    float pZ = sqrt(pZsquare);
-    
-    float plep = mZ/2.;
 
-    float gammaZ = sqrt(mZ*mZ+pZ*pZ)/mZ;
-    float betaZ  = sqrt(1-1/(gammaZ*gammaZ));
+    vector<TLorentzVector> p = Calculate4Momentum(s, mZ, mH, acos(costheta1), acos(costheta2), acos(costhetastar), phi, phi1);
 
-    float gammaH = sqrt(mH*mH+pZ*pZ)/mH;
-    float betaH  = sqrt(1-1/(gammaH*gammaH));
+    difflep1->Fill((p[0].Pt()-ptlminus)/ptlminus);
+    //difflep1->Fill((lepM4vec.Eta()-etalminus)/etalminus);
+    difflep2->Fill((p[1].Pt()-ptlplus)/ptlminus);
+    //difflep2->Fill((lepP4vec.Eta()-etalplus)/etalminus);
 
-    TLorentzVector Z4vec(gammaZ*mZ*betaZ*sqrt(1-costheta1*costheta1),
-			 0.0,
-			 gammaZ*mZ*betaZ*costheta1,
-			 gammaZ*mZ);
-
-    TLorentzVector lepM4vec(.5*mZ*cos(phi)*sqrt(1-costheta2*costheta2),
-			    .5*mZ*sin(phi)*sqrt(1-costheta2*costheta2),
-			    .5*mZ*costheta2,
-			    .5*mZ);
-
-    TLorentzVector lepP4vec(-.5*mZ*cos(phi)*sqrt(1-costheta2*costheta2),
-			    -.5*mZ*sin(phi)*sqrt(1-costheta2*costheta2),
-			    -.5*mZ*costheta2,
-			    .5*mZ);
-    
-    TLorentzRotation ZToZ;
-
-    ZToZ.Boost(0,0,betaZ);
-    ZToZ.RotateY(-TMath::Pi()/2.+costheta1);
-	       
-    lepP4vec = ZToZ*lepP4vec;
-    lepM4vec = ZToZ*lepM4vec;
-    /*
-    cout << " = = = = = = = = = = = = = = = = = " << endl;
-    cout << "costheta1: " << costheta1 << endl;
-    cout << "costheta2: " << costheta2 << endl;
-    cout << "phi: " << phi << endl;
-    cout << "pZ2: " << pZsquare << endl;
-    cout << "pZ: " << pZ << endl;
-    cout << "plep: " << plep << endl;
-
-    cout << "------------------------" << endl;
-    cout << "zp: " << Z4vec.P() << endl;
-    cout << "zpx: " << Z4vec.Px() << endl;
-    cout << "zpy: " << Z4vec.Py() << endl;
-    cout << "zpz: " << Z4vec.Pz() << endl;
-    cout << "zE: "  << Z4vec.E()  << endl;
-
-    cout << "------------------------" << endl;
-    cout << "llpx: " << lepM4vec.Px()+lepP4vec.Px() << endl;
-    cout << "llpy: " << lepM4vec.Py()+lepP4vec.Py() << endl;
-    cout << "llpz: " << lepM4vec.Pz()+lepP4vec.Pz() << endl;
-    cout << "llE: " << lepM4vec.E()+lepP4vec.E() << endl;
-    cout << "------------------------" << endl;
-    cout << "l1px: " << lepM4vec.Px() << endl;
-    cout << "l1py: " << lepM4vec.Py() << endl;
-    cout << "l1pz: " << lepM4vec.Pz() << endl;
-    cout << "l1E: " << lepM4vec.E() << endl;
-    cout << "------------------------" << endl;
-    cout << "l2px: " << lepP4vec.Px() << endl;
-    cout << "l2py: " << lepP4vec.Py() << endl;
-    cout << "l2pz: " << lepP4vec.Pz() << endl;
-    cout << "l2E: " <<  lepP4vec.E() << endl;
-    cout << "------------------------" << endl;
-    
-
-    cout << "ptlplus: "   << ptlplus   << endl;
-    cout << "ptlminus: "  << ptlminus  << endl;
-    cout << "etalplus: "  << etalplus  << endl;
-    cout << "etalminus: " << etalminus << endl;
-    
-    cout << "pt: "  << lepP4vec.Pt()  << endl;
-    cout << "eta: " << lepP4vec.Eta() << endl;
-    cout << "pt: "  << lepM4vec.Pt()  << endl;
-    cout << "eta: " << lepM4vec.Eta() << endl;
-    */
-    //difflep1->Fill((lepM4vec.Pt()-ptlminus)/ptlminus);
-    difflep1->Fill((lepM4vec.Eta()-etalminus)/etalminus);
-    //difflep2->Fill((lepP4vec.Pt()-ptlplus)/ptlminus);
-    difflep2->Fill((lepP4vec.Eta()-etalplus)/etalminus);
   }
 
   difflep1->Draw();
@@ -138,4 +71,67 @@ void calculatePtEta(){
   leg->AddEntry(difflep2,"l^{+}","l");
 
   leg->Draw();
+}
+
+
+vector<TLorentzVector> Calculate4Momentum(float Mx,float M1,float M2,float theta,float theta1,float theta2,float Phi1,float Phi)
+{
+
+  float phi1,phi2;
+  phi1=TMath::Pi()-Phi1;
+  phi2=Phi1+Phi;
+    
+    
+  float gamma1,gamma2,beta1,beta2;
+    
+  gamma1=(Mx*Mx+M1*M1-M2*M2)/(2*Mx*M1);
+  gamma2=(Mx*Mx-M1*M1+M2*M2)/(2*Mx*M2);
+  beta1=sqrt(1-1/(gamma1*gamma1));
+  beta2=sqrt(1-1/(gamma2*gamma2));
+    
+  //gluon 4 vectors
+  TLorentzVector p1CM(0,0,Mx/2,Mx/2);
+  TLorentzVector p2CM(0,0,-Mx/2,Mx/2);
+    
+  //vector boson 4 vectors
+  TLorentzVector kZ1(gamma1*M1*sin(theta)*beta1,0, gamma1*M1*cos(theta)*beta1,gamma1*M1*1);   
+  TLorentzVector kZ2(-gamma2*M2*sin(theta)*beta2,0, -gamma2*M2*cos(theta)*beta2,gamma2*M2*1);
+    
+  //Rotation and Boost matrices. Note gamma1*beta1*M1=gamma2*beta2*M2.
+    
+  TLorentzRotation Z1ToZ,Z2ToZ;
+    
+  Z1ToZ.Boost(0,0,beta1);
+  Z2ToZ.Boost(0,0,beta2);
+  Z1ToZ.RotateY(theta);
+  Z2ToZ.RotateY(TMath::Pi()+theta);
+
+  //fermons 4 vectors in vector boson rest frame
+    
+  TLorentzVector p3Z1((M1/2)*sin(theta1)*cos(phi1),(M1/2)*sin(theta1)*sin(phi1),(M1/2)*cos(theta1),(M1/2)*1);
+       
+  TLorentzVector p4Z1(-(M1/2)*sin(theta1)*cos(phi1),-(M1/2)*sin(theta1)*sin(phi1),-(M1/2)*cos(theta1),(M1/2)*1);
+      
+  TLorentzVector p5Z2((M2/2)*sin(theta2)*cos(phi2),(M2/2)*sin(theta2)*sin(phi2),(M2/2)*cos(theta2),(M2/2)*1);
+    
+  TLorentzVector p6Z2(-(M2/2)*sin(theta2)*cos(phi2),-(M2/2)*sin(theta2)*sin(phi2),-(M2/2)*cos(theta2),(M2/2)*1);
+      
+
+  // fermions 4 vectors in CM frame
+    
+  TLorentzVector p3CM,p4CM,p5CM,p6CM;
+    
+  p3CM=Z1ToZ*p3Z1;
+  p4CM=Z1ToZ*p4Z1;
+  p5CM=Z2ToZ*p5Z2;
+  p6CM=Z2ToZ*p6Z2;
+
+  vector<TLorentzVector> p;
+    
+  p.push_back(p3CM);
+  p.push_back(p4CM);
+  p.push_back(p5CM);
+  p.push_back(p6CM);
+
+  return p;
 }
