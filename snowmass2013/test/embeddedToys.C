ee25f9863c9301e4c6fb125914fb17b744544429
xiaozhou <>
2013-07-02 19:49:19 +0000

    *** empty log message ***

--- snowmass2013/test/embeddedToys.C
+++ snowmass2013/test/embeddedToys.C
@@ -7,7 +7,7 @@ TString sampleName[kNumSamples] = {"fa3p0","fa3p1","fa3p5"};
 enum freeParams {kfa3Only,kfa3phia3Only,kNumScenarios};
 TString scenario[kNumScenarios] = {"fa3_free","fa3_phia3_free"};
 
-TString inputFileNames[kNumSamples] = {"/scratch0/hep/cyou/AllSamples/JHUGen_v3.1.7/0+/rootfiles/SMHiggsToZZTo4L_M-125_14TeV_2e2mu.root",
+TString inputFileNames[kNumSamples] = {"/scratch0/hep/zhouxz/EventGeneration/Samples/14T125GSMHigges/XZ-SMHiggsToZZTo4L_M-125_14TeV.root",
 				       "/scratch0/hep/cyou/AllSamples/JHUGen_v3.1.7/0-_f01/ph0/rootfiles/Higgs0Mf01ph0ToZZTo4L_M-125_14TeV_2e2mu.root",
 				       "/scratch0/hep/cyou/AllSamples/JHUGen_v3.1.7/0-_f05/ph0/rootfiles/Higgs0Mf05ph0ToZZTo4L_M-125_14TeV_2e2mu.root"};
 
@@ -16,17 +16,12 @@ bool fa2Constant [kNumScenarios]   = {true,true};
 bool phia3Constant [kNumScenarios] = {true,false};
 bool phia2Constant [kNumScenarios] = {true,true};
 
-void embeddedToys(int nEvts=50, int nToys=100,
-		  bool runPureToys=false,
+void embeddedToys(int nEvts=21, int nToys=1000,
+		  bool runPureToys=true,
 		  sample mySample = kScalar_fa3p0, 
 		  freeParams myParams = kfa3Only){
-  
-  Playground myPG(125.);
 
-  // load tree to draw toys from
-  myPG.loadTree(inputFileNames[mySample],"SelectedTree");
-
-  // initialize tree to save toys to 
+ // initialize tree to save toys to 
   TTree* results = new TTree("results","toy results");
   
   double fa3,fa3Error;
@@ -42,26 +37,31 @@ void embeddedToys(int nEvts=50, int nToys=100,
   results->Branch("phia3",&phia3,"phia3/D");
   results->Branch("phia3Error",&phia3Error,"phia3Error/D");
   results->Branch("phia2",&phia2,"phia2/D");
+ 
   results->Branch("phia2Error",&phia2Error,"phia2Error/D");
   //---------------------------------
+  
+  Playground myPG(125.);
 
+  // load tree to draw toys from
+  myPG.loadTree(inputFileNames[mySample],"SelectedTree");
 
   // configure parameter for which ones
   // you would like to fit
-  myPG.scalar->fa2->setConstant(fa2Constant[myParams]);
-  myPG.scalar->fa3->setConstant(fa3Constant[myParams]);
-  myPG.scalar->phia2->setConstant(phia2Constant[myParams]);
-  myPG.scalar->phia3->setConstant(phia3Constant[myParams]);
+  myPG.scalar->_modelParams.fa2->setConstant(fa2Constant[myParams]);
+  myPG.scalar->_modelParams.fa3->setConstant(fa3Constant[myParams]);
+  myPG.scalar->_modelParams.phia2->setConstant(phia2Constant[myParams]);
+  myPG.scalar->_modelParams.phia3->setConstant(phia3Constant[myParams]);
   //-----------------------------------
 
 
   for(int i = 0 ; i<nToys ; i++){
 
     // set initial values
-    myPG.scalar->fa2->setVal(0.0);
-    myPG.scalar->fa3->setVal(0.0);
-    myPG.scalar->phia2->setVal(0.0);
-    myPG.scalar->phia3->setVal(0.0);
+    myPG.scalar->_modelParams.fa2->setVal(0.0);
+    myPG.scalar->_modelParams.fa3->setVal(0.0);
+    myPG.scalar->_modelParams.phia2->setVal(0.0);
+    myPG.scalar->_modelParams.phia3->setVal(0.0);
 
     cout << "=======================" << endl;
     cout << "=======================" << endl;
@@ -74,15 +74,15 @@ void embeddedToys(int nEvts=50, int nToys=100,
 
       // get fit results and store to be 
       // saved to TTree
-      fa2 = myPG.scalar->fa2->getVal();
-      fa2Error = myPG.scalar->fa2->getError();
-      fa3 = myPG.scalar->fa3->getVal();
-      fa3Error = myPG.scalar->fa3->getError();
+      fa2 = myPG.scalar->_modelParams.fa2->getVal();
+      fa2Error = myPG.scalar->_modelParams.fa2->getError();
+      fa3 = myPG.scalar->_modelParams.fa3->getVal();
+      fa3Error = myPG.scalar->_modelParams.fa3->getError();
 
-      phia2 = myPG.scalar->phia2->getVal();
-      phia2Error = myPG.scalar->phia2->getError();
-      phia3 = myPG.scalar->phia3->getVal();
-      phia3Error = myPG.scalar->phia3->getError();
+      phia2 = myPG.scalar->_modelParams.phia2->getVal();
+      phia2Error = myPG.scalar->_modelParams.phia2->getError();
+      phia3 = myPG.scalar->_modelParams.phia3->getVal();
+      phia3Error = myPG.scalar->_modelParams.phia3->getError();
 
       // fill TTree
       results->Fill();
@@ -95,7 +95,7 @@ void embeddedToys(int nEvts=50, int nToys=100,
   sprintf(nEvtsString,"_%iEvts",nEvts);
 
   // write tree to output file (ouputFileName set at top)
-  TFile *outputFile = new TFile("embeddedToys_"+sampleName[mySample]+"_"+scenario[myParams]+nEvtsString+".root","RECREATE");
+  TFile *outputFile = new TFile("XZ-pureToys_"+sampleName[mySample]+"_"+scenario[myParams]+nEvtsString+".root","RECREATE");
   results->Write();
   outputFile->Close();
 
