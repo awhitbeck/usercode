83fcd466225f7840a7093598595372e725ed8417
sbologne <>
2013-07-11 12:16:26 +0000

    add background

--- snowmass2013/src/Playground.cc
+++ snowmass2013/src/Playground.cc
@@ -2,7 +2,9 @@
 #define PLAYGROUND
 
 #include "RooSpinZero_7DComplex_withAccep.h"
+#include "RooqqZZ_JHU_ZgammaZZ_fast.h"
 #include "RooPlot.h"
+#include "RooAddPdf.h"
 #include "AngularPdfFactory.cc"
 #include "ScalarPdfFactory.cc"
 #include "RooDataSet.h"
@@ -35,16 +37,26 @@ public:
   vector<RooRealVar*> varContainer;
 
   ScalarPdfFactory* scalar;
+  bool background_;
+  RooqqZZ_JHU_ZgammaZZ_fast* PDF_qqZZ;
+  RooRealVar* upfrac;
+  RooRealVar* bkg_frac;
+  double SoverB_;
+  RooAddPdf* model;
 
   RooDataSet* data;
+  RooDataSet* data_bkg;
+  RooDataSet* data_sigbkg;
   RooDataSet* toyData;
   int embedTracker;
+  int embedTracker_bkg;
 
-  Playground(double mH, bool debug_=false, int parameterization_=2, bool acceptance=false){
+  Playground(double mH, bool debug_=false, int parameterization_=2, bool acceptance=false, bool background=false, double SoverB=0){
     
     debug=debug_;
 
     embedTracker=0;
+    embedTracker_bkg=0;
 
     z1mass = new RooRealVar("Z1Mass","m_{Z1}",90.,12.,120.);
     z2mass = new RooRealVar("Z2Mass","m_{Z2}",25.,12.,120.);
@@ -54,7 +66,7 @@ public:
     phi = new RooRealVar("helphi","#Phi",0.,-TMath::Pi(),TMath::Pi());
     phi1 = new RooRealVar("phistarZ1","#Phi_{1}",0.,-TMath::Pi(),TMath::Pi());
       
-    mzz = new RooRealVar("ZZMass","m_{ZZ}",mH,100,1000);
+    mzz = new RooRealVar("ZZMass","m_{ZZ}",mH,106,141);
 
     varContainer.push_back(z1mass);
     varContainer.push_back(z2mass);
@@ -84,6 +96,20 @@ public:
       scalar->_modelParams.g4Val->setConstant(kFALSE);
       scalar->_modelParams.g4ValIm->setConstant(kFALSE);
     }
+
+    background_=background;
+    if(background_){
+      SoverB_ = SoverB;
+      upfrac = new RooRealVar("upfrac","upfrac",0.5,0.,1.);
+      upfrac->setConstant(kTRUE);
+      //Bakcground fraction over total S+B
+      bkg_frac = new RooRealVar("bkg_frac","bkg_frac",1/(SoverB_+1),0.,10.);
+      bkg_frac->setConstant(kTRUE);
+      PDF_qqZZ= new RooqqZZ_JHU_ZgammaZZ_fast("PDF_qqZZ","PDF_qqZZ",
+					      *z1mass, *z2mass, *costheta1, *costheta2, *phi, *costhetastar, *phi1, *mzz,
+					      *upfrac);
+      model = new RooAddPdf("model","background+signal",*PDF_qqZZ,*(scalar->PDF),*bkg_frac);  
+    }
    }
     
    ~Playground(){
@@ -91,7 +117,8 @@ public:
      //std::cout << "~Playground" << std::endl;
 
     delete scalar;
-    
+    delete PDF_qqZZ;
+
     delete z1mass;
     delete z2mass;
     delete costhetastar;
@@ -102,9 +129,16 @@ public:
     delete mzz;
 
     if(data) delete data;
+    if(data_bkg) delete data_bkg;
+    if(data_sigbkg) delete data_sigbkg;
     //if(toyData) delete toyData;
 
   };
+
+  void setEmbeddingCounter(int newStartingEvent){
+    embedTracker = newStartingEvent;
+    embedTracker_bkg = embedTracker/SoverB_;
+  }
 
   int generate(int nEvents, bool pure=true){
 
@@ -140,6 +174,97 @@ public:
 
   };
 
+  //The user should be careful to pass here a number of events witch match with SoverB parameter of the constructor
+  int generate_bkg(int nEvents, bool pure=true){ 
+
+    if(!background_){
+      cout<<"Error in Playground: you asked to generate bkg but you didn't setup Palyground to have background included!! Aborting..."<<endl;
+      abort();
+    }
+    if(debug)
+      cout << "Playground::generate_bkg()" << endl;
+
+    if(pure)
+      toyData = PDF_qqZZ->generate(RooArgSet(*z1mass,*z2mass,*costhetastar,*costheta1,*costheta2,*phi1,*phi),nEvents);
+    else{
+
+      RooArgSet *tempEvent;
+      toyData = new RooDataSet("toyData","toyData",RooArgSet(*z1mass,*z2mass,*costhetastar,*costheta1,*costheta2,*phi,*phi1));
+
+      if(nEvents+embedTracker_bkg > data->sumEntries()){
+	cout << "Playground::generate_bkg() - ERROR!!! Playground::data does not have enough events to fill toy!!!!  bye :) " << endl;
+	toyData = NULL;
+	return kNotEnoughEvents;
+      }
+
+      for(int iEvent=0; iEvent<nEvents; iEvent++){
+
+	if(debug) cout << "generating bkg event: " << iEvent << " embedTracker_bkg: " << embedTracker_bkg << endl;
+	tempEvent = (RooArgSet*) data_bkg->get(embedTracker_bkg);
+	toyData->add(*tempEvent);
+	embedTracker_bkg++;
+
+      }
+	
+    }
+
+    return kNoError;
+
+  };
+ int generate_sigbkg(int nEvents, bool pure=true){
+
+    if(debug)
+      cout << "Playground::generate_sigbkg()" << endl;
+
+    if(!background_){
+      cout<<"Error in Playground: you asked to generate bkg but you didn't setup Palyground to have background included!! Aborting..."<<endl;
+      abort();
+    }
+
+    if(pure){
+      toyData = scalar->PDF->generate(RooArgSet(*z1mass,*z2mass,*costhetastar,*costheta1,*costheta2,*phi1,*phi),nEvents);
+      toyData->append(*(PDF_qqZZ->generate(RooArgSet(*z1mass,*z2mass,*costhetastar,*costheta1,*costheta2,*phi1,*phi),TMath::Ceil(nEvents/SoverB_))));
+    }
+    else{
+
+      RooArgSet *tempEvent;
+      //if(toyData) delete toyData;
+      toyData = new RooDataSet("toyData","toyData",RooArgSet(*z1mass,*z2mass,*costhetastar,*costheta1,*costheta2,*phi,*phi1));
+
+      if(nEvents+embedTracker > data->sumEntries()){
+	cout << "Playground::generate() - ERROR!!! Playground::data does not have enough events to fill toy!!!!  bye :) " << endl;
+	toyData = NULL;
+	return kNotEnoughEvents;
+      }
+
+      if(nEvents/SoverB_+embedTracker_bkg > data_bkg->sumEntries()){
+	cout << "Playground::generate_bkg() - ERROR!!! Playground::data does not have enough events to fill toy!!!!  bye :) " << endl;
+	toyData = NULL;
+	return kNotEnoughEvents;
+      }
+
+      for(int iEvent=0; iEvent<nEvents; iEvent++){
+
+	if(debug) cout << "generating event: " << iEvent << " embedTracker: " << embedTracker << endl;
+	tempEvent = (RooArgSet*) data->get(embedTracker);
+	toyData->add(*tempEvent);
+	embedTracker++;
+      }
+  
+      for(int iEvent=0; iEvent<nEvents/SoverB_; iEvent++){
+
+	if(debug) cout << "generating bkg event: " << iEvent << " embedTracker_bkg: " << embedTracker_bkg << endl;
+	tempEvent = (RooArgSet*) data_bkg->get(embedTracker_bkg);
+	toyData->add(*tempEvent);
+	embedTracker_bkg++;
+      }
+	
+    }
+
+    return kNoError;
+
+  };
+
   int loadTree(RooDataSet* _data){
 
     data = _data;
@@ -166,6 +291,110 @@ public:
   
   };
 
+
+  int loadTree_bkg(RooDataSet* _data){
+
+    if(!background_){
+      cout<<"Error in Playground: you asked to load bkg but you didn't setup Palyground to have background included!! Aborting..."<<endl;
+      abort();
+    }
+
+    data_bkg = _data;
+    return kNoError;
+
+  }
+
+  int loadTree_bkg(TString fileName, TString treeName){
+
+    if(!background_){
+      cout<<"Error in Playground: you asked to load bkg but you didn't setup Palyground to have background included!! Aborting..."<<endl;
+      abort();
+    }
+
+    TChain* myChain = new TChain(treeName);
+    myChain->Add(fileName);
+    
+    if(!myChain || myChain->GetEntries()<=0) return kFileLoadFailure;
+
+    if(data_bkg)
+      data_bkg=0;
+
+    data_bkg = new RooDataSet("data_bkg","data_bkg",myChain,RooArgSet(*z1mass,*z2mass,*costhetastar,*costheta1,*costheta2,*phi,*phi1),"");
+
+    if(debug)
+      cout << "Number of events in data_bkg: " << data_bkg->numEntries() << endl;
+
+    return kNoError;
+  
+  };
+
+  int loadTree_sigbkg(TString fileName_sig, TString treeName_sig, TString fileName_bkg, TString treeName_bkg){
+
+    TChain* myChain_sig = new TChain(treeName_sig);
+    myChain_sig->Add(fileName_sig);
+    TChain* myChain_bkg = new TChain(treeName_bkg);
+    myChain_bkg->Add(fileName_bkg);
+
+    int maxSig = myChain_sig->GetEntries();
+    int maxBkg = myChain_bkg->GetEntries();
+    int sigEvts=0, bkgEvts=0;
+    
+    if(maxSig < maxBkg*SoverB_){
+      bkgEvts=maxSig*(1/SoverB_);
+      sigEvts=maxSig;
+    }
+    else{
+      bkgEvts=maxBkg;
+      sigEvts=maxBkg*SoverB_;
+    }
+
+    TTree* newtree = (TTree*)myChain_bkg->GetTree()->CloneTree(0);
+
+    Int_t nbytes = 0, nb = 0, tnumber = -1;
+    for (Int_t jentry=0; jentry<bkgEvts;jentry++) {
+      nb = myChain_bkg->GetEntry(jentry);   nbytes += nb;
+
+      if (tnumber != myChain_bkg->GetTreeNumber()) {
+         tnumber = myChain_bkg->GetTreeNumber();
+         myChain_bkg->CopyAddresses(newtree);
+      }
+
+      newtree->Fill();
+   }
+
+    for (Int_t jentry=0; jentry<sigEvts;jentry++) {
+      nb = myChain_sig->GetEntry(jentry);   nbytes += nb;
+
+      if (tnumber != myChain_sig->GetTreeNumber()) {
+         tnumber = myChain_sig->GetTreeNumber();
+         myChain_sig->CopyAddresses(newtree);
+      }
+
+      newtree->Fill();
+   }
+
+   data_sigbkg = new RooDataSet("data_sigbkg","data_sigbkg",newtree,RooArgSet(*z1mass,*z2mass,*costhetastar,*costheta1,*costheta2,*phi,*phi1),"");
+ 
+   if(debug){
+      cout << "Number of events in data_sigbkg: " << data_sigbkg->numEntries() << endl;
+      cout<< "Number of signal: "<<sigEvts<<"   -   number of background "<<bkgEvts<<endl;
+   }
+    return kNoError;
+   }
+
+  int loadTree_sigbkg(RooDataSet* _data){
+
+    if(!background_){
+      cout<<"Error in Playground: you asked to load sig+bkg but you didn't setup Palyground to have background included!! Aborting..."<<endl;
+      abort();
+    }
+
+    data_sigbkg = _data;
+    return kNoError;
+
+  }
+
+
   RooFitResult* fitData(bool istoy = false, int PrintLevel = 1){
 
     if ( istoy )  {
@@ -173,6 +402,38 @@ public:
     }
     else  {
       return ((scalar->PDF)->fitTo(*data, RooFit::PrintLevel(PrintLevel), RooFit::Save(true))); 
+    }
+    
+  };
+
+  RooFitResult* fitData_bkg(bool istoy = false, int PrintLevel = 1){
+
+    if(!background_){
+      cout<<"Error in Playground: you asked to fit sig+bkg but you didn't setup Palyground to have background included!! Aborting..."<<endl;
+      abort();
+    }
+
+    if ( istoy )  {
+      return (PDF_qqZZ->fitTo(*toyData, RooFit::PrintLevel(PrintLevel), RooFit::Save(true)));
+    }
+    else  {
+      return (PDF_qqZZ->fitTo(*data_bkg, RooFit::PrintLevel(PrintLevel), RooFit::Save(true))); 
+    }
+    
+  };
+
+  RooFitResult* fitData_sigbkg(bool istoy = false, int PrintLevel = 1){
+
+    if(!background_){
+      cout<<"Error in Playground: you asked to fit bkg but you didn't setup Palyground to have background included!! Aborting..."<<endl;
+      abort();
+    }
+
+    if ( istoy )  {
+      return (model->fitTo(*toyData, RooFit::PrintLevel(PrintLevel), RooFit::Save(true)));
+    }
+    else  {
+      return (model->fitTo(*data_sigbkg, RooFit::PrintLevel(PrintLevel), RooFit::Save(true))); 
     }
     
   };
@@ -193,6 +454,67 @@ public:
     }
 
     scalar->PDF->plotOn(plot);
+    
+    plot->Draw();
+
+    return kNoError;
+
+  }
+
+  int projectPDF_sigbkg(varList myVar, int bins=20, bool istoy=false){
+
+    if(!background_){
+      cout<<"Error in Playground: you asked to project sig+bkg but you didn't setup Palyground to have background included!! Aborting..."<<endl;
+      abort();
+    }
+
+    if(!data_sigbkg && !istoy){
+      cout<<"Error in Playground: you asked to project sig+bkg but you need first to call loadTree_sigbkg"<<endl;
+      abort();
+    }
+
+    if(debug) cout << "Playground::projectionPDF()" << endl;
+    RooPlot* plot = varContainer[myVar]->frame(bins);
+
+    if(debug) cout << "RooPlot: " << plot << endl;
+
+    if ( istoy ) {
+      if( !toyData ) return kDataEmpty; 
+      toyData->plotOn(plot);
+    }else{
+      if( !data_sigbkg ) return kDataEmpty;
+      data_sigbkg->plotOn(plot);
+    }
+
+    model->plotOn(plot);
+    
+    plot->Draw();
+
+    return kNoError;
+
+  }
+
+  int projectPDF_bkg(varList myVar, int bins=20, bool istoy=false){
+
+    if(!background_){
+      cout<<"Error in Playground: you asked to project bkg but you didn't setup Palyground to have background included!! Aborting..."<<endl;
+      abort();
+    }
+
+    if(debug) cout << "Playground::projectionPDF()" << endl;
+    RooPlot* plot = varContainer[myVar]->frame(bins);
+
+    if(debug) cout << "RooPlot: " << plot << endl;
+
+    if ( istoy ) {
+      if( !toyData ) return kDataEmpty; 
+      toyData->plotOn(plot);
+    }else{
+      if( !data_bkg ) return kDataEmpty;
+      data_bkg->plotOn(plot);
+    }
+
+    PDF_qqZZ->plotOn(plot);
     
     plot->Draw();
 
--- snowmass2013/test/embeddedToys.C
+++ snowmass2013/test/embeddedToys.C
@@ -1,5 +1,7 @@
 #include "../src/Playground.cc"
 
+//[#0] ERROR:InputArguments -- RooAddPdf::getAnalyticalIntegral(model) WARNING: component PDF PDF   advertises inconsistent set of integrals (e.g. (X,Y) but not X or Y individually.   Distributed analytical integration disabled. Please fix PDF
+
 using namespace PlaygroundHelpers;
 
 enum sample {kScalar_fa3p0,kScalar_fa3p1,kScalar_fa3p5,kScalar_fa3p5phia390,kScalar_fa3p25,kScalar_fa2p1fa3p1,kNumSamples};
@@ -7,12 +9,20 @@ TString sampleName[kNumSamples] = {"fa3p0","fa3p1","fa3p5","fa3p5phia390","fa3p2
 enum freeParams {kfa3Only,kfa3phia3Only,kfa2Only,kfa2phia2Only,kfa2fa3Only,kAllFree,kNumScenarios};
 TString scenario[kNumScenarios] = {"fa3_free","fa3_phia3_free","fa2_free","fa2_phia2_free","fa2_fa3_free","all_free"};
 
-TString inputFileNames[kNumSamples] = {"/afs/cern.ch/user/s/sbologne/workspace/multidimFit_newSamples/ZZMatrixElement/snowmass2013/test/SMHiggsToZZTo4L_M-125_14TeV_2e2mu_withProbabilities.root",
-				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_newSamples/ZZMatrixElement/snowmass2013/test/Higgs0Mf01ph0ToZZTo4L_M-125_14TeV_2e2mu_withProbabilities.root",
-				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_newSamples/ZZMatrixElement/snowmass2013/test/Higgs0Mf05ph0ToZZTo4L_M-125_14TeV_2e2mu_withProbabilities.root",
-				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_newSamples/ZZMatrixElement/snowmass2013/test/Higgs0Mf05ph90ToZZTo4L_M-125_14TeV_2e2mu_withProbabilities.root",
-				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_newSamples/ZZMatrixElement/snowmass2013/test/XZ-Higgs0Mf025ph0ToZZTo4l_M-125_14TeV_withDiscriminants_2e2mu_withProbabilities.root",
-				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_newSamples/ZZMatrixElement/snowmass2013/test/Higgs0Mf01ph0Higgs0PHf01ph0ToZZTo4L_M-125_14TeV_withDiscriminants_2e2mu_withProbabilities.root"};
+TString inputFileNames[kNumSamples] = {"/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/JHUpsMELA_m2cut_2e2mu/SMHiggsToZZTo4L_M-125_14TeV_2e2mu_withProbabilities.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/JHUpsMELA_m2cut_2e2mu/Higgs0Mf01ph0ToZZTo4L_M-125_14TeV_2e2mu_withProbabilities.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/JHUpsMELA_m2cut_2e2mu/Higgs0Mf05ph0ToZZTo4L_M-125_14TeV_2e2mu_withProbabilities.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/JHUpsMELA_m2cut_2e2mu/Higgs0Mf05ph90ToZZTo4L_M-125_14TeV_2e2mu_withProbabilities.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/analyticalpsMELA/m2cut_2e2mu/XZ-Higgs0Mf025ph0ToZZTo4l_M-125_14TeV_withDiscriminants_2e2mu.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/JHUpsMELA_m2cut_2e2mu/Higgs0Mf01ph0Higgs0PHf01ph0ToZZTo4L_M-125_14TeV_withDiscriminants_2e2mu_withProbabilities.root"};
+/*
+TString inputFileNames[kNumSamples] = {"/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/analyticalpsMELA/m2cut_2e2mu/SMHiggsToZZTo4L_M-125_14TeV_withDiscriminants_2e2mu.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/analyticalpsMELA/m2cut_2e2mu/Higgs0Mf01ph0ToZZTo4L_M-125_14TeV_withDiscriminants_2e2mu.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/analyticalpsMELA/m2cut_2e2mu/Higgs0Mf05ph0ToZZTo4L_M-125_14TeV_withDiscriminants_2e2mu.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/analyticalpsMELA/m2cut_2e2mu/Higgs0Mf05ph90ToZZTo4L_M-125_14TeV_withDiscriminants_2e2mu.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/analyticalpsMELA/m2cut_2e2mu/XZ-Higgs0Mf025ph0ToZZTo4l_M-125_14TeV_withDiscriminants_2e2mu.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/analyticalpsMELA/m2cut_2e2mu/Higgs0Mf01ph0Higgs0PHf01ph0ToZZTo4L_M-125_14TeV_withDiscriminants_2e2mu.root"};
+*/
 
 bool fa3Constant [kNumScenarios]   = {false,false,true, true, false,false};
 bool fa2Constant [kNumScenarios]   = {true, true, false,false,false,false};
@@ -20,11 +30,12 @@ bool phia3Constant [kNumScenarios] = {true, false,true, true, true, false};
 bool phia2Constant [kNumScenarios] = {true, true, true, false,true, false};
 
 
-void embeddedToys(int nEvts=21, int nToys=1000,
+void embeddedToys(int nEvts=50, int nToys=10,
 		  bool runPureToys=false,
-		  sample mySample = kScalar_fa3p0, 
-		  freeParams myParams = kfa3Only){
-
+		  sample mySample = kScalar_fa3p5, 
+		  freeParams myParams = kfa3Only,
+		  bool bkg=false, int iteration=0){
+  RooRandom::randomGenerator()->SetSeed(iteration+1*129456) ;
  // initialize tree to save toys to 
   TTree* results = new TTree("results","toy results");
   
@@ -45,10 +56,13 @@ void embeddedToys(int nEvts=21, int nToys=1000,
   results->Branch("phia2Error",&phia2Error,"phia2Error/D");
   //---------------------------------
   
-  Playground myPG(125.);
+  Playground myPG(125., true, 2, false, bkg,3.75);
+  myPG.setEmbeddingCounter(iteration*nEvts*nToys);
 
   // load tree to draw toys from
   myPG.loadTree(inputFileNames[mySample],"SelectedTree");
+  if(bkg)
+    myPG.loadTree_bkg("/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/analyticalpsMELA/m2cut_2e2mu/pwgevents_hadd_withDiscriminants_2e2mu.root","SelectedTree");
 
   // configure parameter for which ones
   // you would like to fit
@@ -85,35 +99,48 @@ void embeddedToys(int nEvts=21, int nToys=1000,
     cout << "=======================" << endl;
     cout << "=========" << i << "========" << endl;
 
-    if(myPG.generate(nEvts,runPureToys)==kNoError){
-
+    if(bkg){
+      myPG.generate_sigbkg(nEvts,runPureToys);
+      // perform fit
+      myPG.fitData_sigbkg(true,-1);
+    }
+    else{
+      myPG.generate(nEvts,runPureToys);
       // perform fit
       myPG.fitData(true,-1);
-
-      // get fit results and store to be 
-      // saved to TTree
-      fa2 = myPG.scalar->_modelParams.fa2->getVal();
-      fa2Error = myPG.scalar->_modelParams.fa2->getError();
-      fa3 = myPG.scalar->_modelParams.fa3->getVal();
-      fa3Error = myPG.scalar->_modelParams.fa3->getError();
-
-      phia2 = myPG.scalar->_modelParams.phia2->getVal();
-      phia2Error = myPG.scalar->_modelParams.phia2->getError();
-      phia3 = myPG.scalar->_modelParams.phia3->getVal();
-      phia3Error = myPG.scalar->_modelParams.phia3->getError();
-
-      // fill TTree
-      results->Fill();
-
     }
+
+    // get fit results and store to be 
+    // saved to TTree
+    fa2 = myPG.scalar->_modelParams.fa2->getVal();
+    fa2Error = myPG.scalar->_modelParams.fa2->getError();
+    fa3 = myPG.scalar->_modelParams.fa3->getVal();
+    fa3Error = myPG.scalar->_modelParams.fa3->getError();
+
+    phia2 = myPG.scalar->_modelParams.phia2->getVal();
+    phia2Error = myPG.scalar->_modelParams.phia2->getError();
+    phia3 = myPG.scalar->_modelParams.phia3->getVal();
+    phia3Error = myPG.scalar->_modelParams.phia3->getError();
+
+    // fill TTree
+    results->Fill();
 
   }
 
   char nEvtsString[100];
-  sprintf(nEvtsString,"_%iEvts",nEvts);
+  if(bkg)
+    sprintf(nEvtsString,"_%iEvtsPlusBkg_%iter",nEvts,iteration);
+  else
+    sprintf(nEvtsString,"_%iEvts_%iter",nEvts,iteration);
 
   // write tree to output file (ouputFileName set at top)
-  TFile *outputFile = new TFile("embeddedToys_"+sampleName[mySample]+"_"+scenario[myParams]+nEvtsString+".root","RECREATE");
+  TString outputFileName;
+  if(runPureToys)
+    outputFileName="pureToys_"+sampleName[mySample]+"_"+scenario[myParams]+nEvtsString+".root";
+  else
+    outputFileName="embeddedToys_"+sampleName[mySample]+"_"+scenario[myParams]+nEvtsString+".root";
+
+  TFile *outputFile = new TFile(outputFileName,"RECREATE");
   results->Write();
   outputFile->Close();
 
