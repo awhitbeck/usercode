47e240c044f71ef6e5208a142809e15ee447c372
sbologne <>
2013-07-23 16:38:39 +0000

    leatest version

--- snowmass2013/test/embeddedToys.C
+++ snowmass2013/test/embeddedToys.C
@@ -4,8 +4,8 @@
 
 using namespace PlaygroundHelpers;
 
-enum sample {kScalar_fa3p0,kScalar_fa3p1,kScalar_fa3p5,kScalar_fa3p5phia390,kScalar_fa3p25,kScalar_fa2p1fa3p1,kScalar_fa3p06,kScalar_fa3p18,kNumSamples};
-TString sampleName[kNumSamples] = {"fa3p0","fa3p1","fa3p5","fa3p5phia390","fa3p25","fa2p1fa3p1","fa3p06","fa3p18"};
+enum sample {kScalar_fa3p0,kScalar_fa3p1,kScalar_fa3p5,kScalar_fa3p5phia390,kScalar_fa3p25,kScalar_fa2p1fa3p1,kScalar_fa3p06,kScalar_fa3p18,kScalar_fa2p06,kScalar_fa2p18,kScalar_fa2p06fa3p06,kScalar_fa2p18fa3p18,kNumSamples};
+TString sampleName[kNumSamples] = {"fa3p0","fa3p1","fa3p5","fa3p5phia390","fa3p25","fa2p1fa3p1","fa3p06","fa3p18","fa2p06","fa2p18","fa2p06fa3p06","fa2p18fa3p18"};
 enum freeParams {kfa3Only,kfa3phia3Only,kfa2Only,kfa2phia2Only,kfa2fa3Only,kAllFree,kNumScenarios};
 TString scenario[kNumScenarios] = {"fa3_free","fa3_phia3_free","fa2_free","fa2_phia2_free","fa2_fa3_free","all_free"};
 
@@ -15,10 +15,12 @@ TString inputFileNames[kNumSamples] = {"/afs/cern.ch/user/s/sbologne/workspace/m
 				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/JHUpsMELA_m2cut_2e2mu/Higgs0Mf05ph90ToZZTo4L_M-125_14TeV_2e2mu_withProbabilities.root",
 				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/analyticalpsMELA/m2cut_2e2mu/XZ-Higgs0Mf025ph0ToZZTo4l_M-125_14TeV_withDiscriminants_2e2mu.root",
 				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/JHUpsMELA_m2cut_2e2mu/Higgs0Mf01ph0Higgs0PHf01ph0ToZZTo4L_M-125_14TeV_withDiscriminants_2e2mu_withProbabilities.root",
-
-				       //dummy for now!!!
-				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/JHUpsMELA_m2cut_2e2mu/SMHiggsToZZTo4L_M-125_14TeV_2e2mu_withProbabilities.root",
-				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/JHUpsMELA_m2cut_2e2mu/SMHiggsToZZTo4L_M-125_14TeV_2e2mu_withProbabilities.root"};
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/detEffects/XZ-Higgs0Mf006ph0ToZZTo4l_M-125_14TeV_wResolution_withDiscriminants_2e2mu.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/detEffects/Higgs0Mf018ToZZTo4L_M-125_14TeV_wResolution_withDiscriminants_2e2mu.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/detEffects/XZ-Higgs0Hf006ph0ToZZTo4l_M-125_14TeV_wResolution_withDiscriminants_2e2mu.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/detEffects/Higgs0PHf018ToZZTo4L_M-125_14TeV_wResolution_withDiscriminants_2e2mu.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/detEffects/XZ-Higgs0Mf006ph0Higgs0PHf006ph0ToZZTo4L_M-125_14TeV_wResolution_withDiscriminants_2e2mu.root",
+				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/detEffects/Higgs0Mf018Higgs0PHf018ToZZTo4L_M-125_14TeV_wResolution_withDiscriminants_2e2mu.root"};
 /*
 TString inputFileNames[kNumSamples] = {"/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/analyticalpsMELA/m2cut_2e2mu/SMHiggsToZZTo4L_M-125_14TeV_withDiscriminants_2e2mu.root",
 				       "/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/analyticalpsMELA/m2cut_2e2mu/Higgs0Mf01ph0ToZZTo4L_M-125_14TeV_withDiscriminants_2e2mu.root",
@@ -39,7 +41,7 @@ void embeddedToys(int nEvts=50, int nToys=10,
 		  sample mySample = kScalar_fa3p5, 
 		  freeParams myParams = kfa3Only,
 		  bool bkg=false, int iteration=0){
-  RooRandom::randomGenerator()->SetSeed(iteration+1*129456) ;
+  RooRandom::randomGenerator()->SetSeed(iteration+127853) ;
  // initialize tree to save toys to 
   TTree* results = new TTree("results","toy results");
   
@@ -59,17 +61,19 @@ void embeddedToys(int nEvts=50, int nToys=10,
  
   results->Branch("phia2Error",&phia2Error,"phia2Error/D");
   //---------------------------------
-  
-  Playground myPG(125., true, 2, false, bkg,2.5);
+
+  Playground myPG(125., true, 2, true, bkg,2.5);  //ACCEPTANCE SWITCHED ON!!!
   myPG.setEmbeddingCounter(iteration*nEvts*nToys);
 
   // load tree to draw toys from
   myPG.loadTree(inputFileNames[mySample],"SelectedTree");
+    cout<<"prova"<<endl;
   if(bkg)
-    myPG.loadTree_bkg("/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/analyticalpsMELA/m2cut_2e2mu/pwgevents_hadd_withDiscriminants_2e2mu_mzz120130.root","SelectedTree");
+    myPG.loadTree_bkg("/afs/cern.ch/user/s/sbologne/workspace/multidimFit_prophecy/ZZMatrixElement/snowmass2013/test/samples/detEffects/pwgevents_hadd_wResolution_withDiscriminants_2e2mu.root","SelectedTree");
 
   // configure parameter for which ones
   // you would like to fit
+    // set initial values
   myPG.scalar->_modelParams.fa2->setConstant(fa2Constant[myParams]);
   myPG.scalar->_modelParams.fa3->setConstant(fa3Constant[myParams]);
   myPG.scalar->_modelParams.phia2->setConstant(phia2Constant[myParams]);
@@ -78,12 +82,11 @@ void embeddedToys(int nEvts=50, int nToys=10,
 
 
   for(int i = 0 ; i<nToys ; i++){
-
-    // set initial values
     myPG.scalar->_modelParams.fa2->setVal(0.0);
     myPG.scalar->_modelParams.fa3->setVal(0.0);
     myPG.scalar->_modelParams.phia2->setVal(0.0);
     myPG.scalar->_modelParams.phia3->setVal(0.0);
+
     if(mySample == kScalar_fa3p1)
       myPG.scalar->_modelParams.fa3->setVal(0.1);
     if(mySample == kScalar_fa3p25)
@@ -102,6 +105,20 @@ void embeddedToys(int nEvts=50, int nToys=10,
       myPG.scalar->_modelParams.fa3->setVal(0.06);
     }
     if(mySample == kScalar_fa3p18){
+      myPG.scalar->_modelParams.fa3->setVal(0.18);
+    }
+    if(mySample == kScalar_fa2p06){
+      myPG.scalar->_modelParams.fa2->setVal(0.06);
+    }
+    if(mySample == kScalar_fa2p18){
+      myPG.scalar->_modelParams.fa2->setVal(0.18);
+    }
+    if(mySample == kScalar_fa2p06fa3p06){
+      myPG.scalar->_modelParams.fa2->setVal(0.06);
+      myPG.scalar->_modelParams.fa3->setVal(0.06);
+    }
+    if(mySample == kScalar_fa2p18fa3p18){
+      myPG.scalar->_modelParams.fa2->setVal(0.18);
       myPG.scalar->_modelParams.fa3->setVal(0.18);
     }
  
