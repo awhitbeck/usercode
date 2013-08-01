1b9e84f335946e7c1e592b226eee9f27fe8c7151
awhitbeck <whitbeck.andrew@gmail.com>
2013-08-01 14:38:10 -0400

    adding new cross sections to fa3converter for VBF, H2F, and ILC1000

--- snowmass2013/src/fa3Converter.cc
+++ snowmass2013/src/fa3Converter.cc
@@ -5,10 +5,13 @@ using namespace std;
 
 enum process {kLHC_HZZs_mH125,
 	      kLHC_HZZs_mH126,
+	      kLHC_VBF_14TeV_mH126,
+	      kLHC_H2J_14TeV_mH126,
 	      kILC_250_mH125,
 	      kILC_250_mH126,
 	      kILC_350,
 	      kILC_500,
+	      kILC_1000,
 	      kNUMconvert};
 
 class fa3Converter {
@@ -39,25 +42,34 @@ fa3Converter::fa3Converter(process _from, process _to ){
 
   sigma1.push_back(1.860351);  // kLHC_HZZs_mH125 
   sigma1.push_back(2.03971);   // kLHC_HZZs_mH126 
+  sigma1.push_back(4687.9);    // kLHC_VBF_14TeV_mH126
+  sigma1.push_back(6783.68);   // kLHC_VBF_14TeV_mH126
   sigma1.push_back(0.981396);  // kILC_250_mH125  
   sigma1.push_back(0.77517);   // kILC_250_mH126  
   sigma1.push_back(1.48872);   // kILC_350	  
   sigma1.push_back(2.57246);   // kILC_500        
+  sigma1.push_back(8.95721);   // kILC_1000        
 
   sigma2.push_back(0.672859);  // kLHC_HZZs_mH125 
   sigma2.push_back(0.77517);   // kLHC_HZZs_mH126 
+  sigma2.push_back(0.0);       // kLHC_VBF_14TeV_mH126
+  sigma2.push_back(0.0);       // kLHC_VBF_14TeV_mH126
   sigma2.push_back(33.4674);   // kILC_250_mH125  
   sigma2.push_back(32.1981);   // kILC_250_mH126  
   sigma2.push_back(125.387);   // kILC_350	  
   sigma2.push_back(516.556);   // kILC_500        
+  sigma2.push_back(8208.91);   // kILC_1000        
 
   sigma4.push_back(0.284353);  // kLHC_HZZs_mH125 
   sigma4.push_back(0.32689);   // kLHC_HZZs_mH126 
+  sigma4.push_back(84932.1);   // kLHC_VBF_14TeV_mH126
+  sigma4.push_back(6583.72);   // kLHC_VBF_14TeV_mH126
   sigma4.push_back(7.9229);    // kILC_250_mH125  
   sigma4.push_back(7.45502);   // kILC_250_mH126  
   sigma4.push_back(75.3199);   // kILC_350	  
   sigma4.push_back(414.378);   // kILC_500        
-  
+  sigma4.push_back(7800.2);    // kILC_1000        
+
 };
 
 double fa3Converter::getConverted(double fa3){
