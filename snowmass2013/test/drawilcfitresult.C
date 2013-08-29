a0563ad0c26c04996a58e9d01d4ceae0a391dfec
yanyangao <yyg.gao@gmail.com>
2013-08-29 16:00:12 -0500

    update

--- snowmass2013/test/drawilcfitresult.C
+++ snowmass2013/test/drawilcfitresult.C
@@ -34,48 +34,77 @@
 bool debug = false;
 
 
-typedef enum {kILC, kLHC} colliderType;
-typedef enum{kF3, kFA3} varLevel; 
+typedef enum {kILC, kLHCDECAY, kLHCZH} colliderType;
+typedef enum{kF3, kFA3, kF3ACC} varLevel; 
+typedef enum{kOneD, kTwoD, kALL} plotLevel; 
+typedef enum{EMBED, PURE} toyLevel; 
 
 void drawilcfitresult()
 {
 
   int nBins = 40;
-  colliderType collider = kILC; 
-  bool withAcceptance = false;
+  colliderType collider = kLHCZH;
+  plotLevel plot = kOneD;
+  toyLevel toy = PURE;
+  bool withAcceptance = true;
+  
   TString accName = "false";
   if (withAcceptance )  accName = "true";
+  
+  TString toyMode = "pure";
+  if ( toy == EMBED ) toyMode = "embd";
 
-  // set the f3 value 
-  double f2gen = 0.;
-  double f3gen = 0.;
   
-  if ( collider == kILC ) {
-    f2gen = 0.;
-    f3gen = 0.1;
-  }
-  
-  vector<float> energies;
-  energies.push_back(250.);
-  energies.push_back(350.);
-  energies.push_back(500.);
-  energies.push_back(1000.);
-  
-  //
-  // 1D Plots
-  // 
-  
-  for ( int i = 0; i < energies.size() ; i ++ ) {
-    float sqrtsVal = energies.at(i);
-    TString toyName = Form("embd_f_3_%.0fGeV_acc%s", sqrtsVal, accName.Data());
-    drawsingle(sqrtsVal, f2gen, f3gen, toyName, kF3, collider, Form("f_{3}"), nBins);
-    drawsingle(sqrtsVal, f2gen, f3gen, toyName, kFA3, collider, Form("f_{a3}"), nBins);
-  }
-  
-  // 2D plots
-  draw2dsingle("pure_f_2_p15_f_3_250GeV_accfalse", "fa2", "fa3", "f_{2}", "f_{3}", 50, 0, 0.4, 50, 0, 0.4, 0.15, 0.1);
-  draw2dsingle("pure_f_3_250GeV_accfalse", "fa3", "phia3",  "f_{3}", "#phi_{3}", 50, 0, 0.4, 50, -1.6, 1.6, 0.1, 0);
+  if ( plot == kOneD || plot == kALL ) {
     
+    // set the f3 value 
+    double f2gen = 0.;
+    double f3gen = 0.;
+    
+    // 
+    // ILC plots 
+    // 
+    if ( collider == kILC ) {
+      f2gen = 0.;
+      f3gen = 0.1;
+      
+      vector<float> energies;
+      energies.push_back(250.);
+      energies.push_back(350.);
+      energies.push_back(500.);
+      energies.push_back(1000.);
+      
+      //
+      // 1D Plots
+      // 
+      
+      for ( int i = 0; i < energies.size() ; i ++ ) {
+	float sqrtsVal = energies.at(i);
+	TString toyName = Form("%s_f_3_%.0fGeV_acc%s", toyMode.Data(), sqrtsVal, accName.Data());
+	drawsingle(sqrtsVal, f2gen, f3gen, toyName, kF3, collider, Form("f_{3}"), nBins);
+	// drawsingle(sqrtsVal, f2gen, f3gen, toyName, kFA3, collider, Form("f_{a3}"), nBins);
+      }
+    } // end of ILC plots
+
+    if ( collider == kLHCZH ) {
+      float sqrtsVal = 14000;
+      float lumi = 300; 
+      TString toyName = Form("KD_%s_%.0fGeV_acctrue_%.0fifb", toyMode.Data(), sqrtsVal, lumi);
+      
+      if ( lumi == 3000. ) f3gen = 0.385797;
+      if ( lumi == 300. ) f3gen = 0.5; 
+      //drawsingle(sqrtsVal, f2gen, f3gen, toyName, kF3, collider, Form("f_{3}"), nBins);
+      drawsingle(sqrtsVal, f2gen, f3gen, toyName, kF3ACC, collider, Form("f_{3}"), nBins);
+    }
+  }
+
+  
+
+  if ( plot == kTwoD || plot == kALL ) {
+    // 2D plots
+    draw2dsingle("pure_f_2_p15_f_3_250GeV_accfalse", "fa2", "fa3", "f_{2}", "f_{3}", 50, 0, 0.4, 50, 0, 0.4, 0.15, 0.1);
+    draw2dsingle("pure_f_3_250GeV_accfalse", "fa3", "phia3",  "f_{3}", "#phi_{3}", 50, 0, 0.4, 50, -1.6, 1.6, 0.1, 0);
+  }
   
 }
 
@@ -89,6 +118,8 @@ void drawsingle(float sqrtsVal, float f2gen, float f3gen, TString toyName, int v
   
   float fa2gen(0.);
   float fa3gen(0.);
+  if ( collider == kLHCZH && variable == kF3ACC) 
+    f3gen = calcfacc(f3gen);
   calcgcoup(f2gen, f3gen, sqrtsVal, collider, fa2gen, fa3gen);
 
   float xgen = 0.;
@@ -104,6 +135,11 @@ void drawsingle(float sqrtsVal, float f2gen, float f3gen, TString toyName, int v
     var = "fa3";
     xgen = fa3gen;
   }
+
+  if ( variable == kF3ACC ) {
+    var = "f3_aftercut";
+    xgen = f3gen;    
+  }
   
   // assume the expected err is 1/3 of the generated values
   xerr = xgen / 3.;
@@ -114,7 +150,12 @@ void drawsingle(float sqrtsVal, float f2gen, float f3gen, TString toyName, int v
   TH1F *h_var = new TH1F("h_var", "h_var", nBins, xMin, xMax);
   TH1F *h_var_pull = new TH1F("h_var_pull", "h_var_pull", 40, -4, 4);
 
-  TString fileName = Form("toyresults_ilc/toyresults_%s.root", toyName.Data());
+  TString toyDir = "toyresults_ilc/";
+  
+  if ( collider == kLHCZH ) 
+    toyDir = "toyresults_ppzh";
+
+  TString fileName = Form("%s/toyresults_%s.root", toyDir.Data(), toyName.Data());
   TFile *file = new TFile(fileName, "READ");
   gROOT->cd();
   std::cout << "Opening " << fileName << "\n";
@@ -132,31 +173,53 @@ void drawsingle(float sqrtsVal, float f2gen, float f3gen, TString toyName, int v
   float phi3_err_ = 0.;
   float phi3_pull_ = 0.;
 
-  tree->SetBranchAddress("fa2"        , &f2_        );
-  tree->SetBranchAddress("fa2_err"    , &f2_err_    );
-  tree->SetBranchAddress("fa2_pull"   , &f2_pull_   );
-  tree->SetBranchAddress("fa3"        , &f3_        );
-  tree->SetBranchAddress("fa3_err"    , &f3_err_    );
-  tree->SetBranchAddress("fa3_pull"   , &f3_pull_   );
-  tree->SetBranchAddress("phia3"      , &phi3_      );
-  tree->SetBranchAddress("phia3_err"  , &phi3_err_  );
+  if ( tree->GetBranchStatus("fa2") )
+    tree->SetBranchAddress("fa2"        , &f2_        );
+  if ( tree->GetBranchStatus("fa2_err") )
+    tree->SetBranchAddress("fa2_err"    , &f2_err_    );
+  if ( tree->GetBranchStatus("fa2_pull") )
+    tree->SetBranchAddress("fa2_pull"   , &f2_pull_   );
+  if ( tree->GetBranchStatus("fa3") )
+    tree->SetBranchAddress("fa3"        , &f3_        );
+  if ( tree->GetBranchStatus("fa3_err") )
+    tree->SetBranchAddress("fa3_err"    , &f3_err_    );
+  if ( tree->GetBranchStatus("fa3_pull") )
+    tree->SetBranchAddress("fa3_pull"   , &f3_pull_   );
+  if ( tree->GetBranchStatus("phia3") )
+    tree->SetBranchAddress("phia3"      , &phi3_      );
+  if ( tree->GetBranchStatus("phia3_err") )
+    tree->SetBranchAddress("phia3_err"  , &phi3_err_  );
+  if ( tree->GetBranchStatus("phia3_pull") )
   tree->SetBranchAddress("phia3_pull" , &phi3_pull_ );
   
   for(int ievt = 0; ievt < tree->GetEntries() ;ievt++){
     tree->GetEntry(ievt); 
     
-    if ( variable == kF3 ) {
-      h_var->Fill(f3_);
-      h_var_pull->Fill(f3_pull_);
+    if ( collider == kLHCZH) {
+      if ( variable == kF3 ) {
+	double f3_bf_acc = calcfacc(f3_);
+	h_var->Fill(f3_bf_acc);
+	//h_var->Fill(f3_);
+	h_var_pull->Fill(f3_pull_);
+      } else if ( variable == kF3ACC ) {
+	h_var->Fill(f3_);
+	h_var_pull->Fill(f3_pull_);
+      }
+    } else {
+      if ( variable == kF3 ) {
+	h_var->Fill(f3_);
+	h_var_pull->Fill(f3_pull_);
+      }
+      
+      if ( variable == kFA3 ) {
+	float fa2_ = 0.;
+	float fa3_ = 0.;
+	calcgcoup(f2_, f3_, sqrtsVal, kILC, fa2_, fa3_);
+	h_var->Fill(fa3_);
+	h_var_pull->Fill(f3_pull_);
+      }
     }
-
-    if ( variable == kFA3 ) {
-      float fa2_ = 0.;
-      float fa3_ = 0.;
-      calcgcoup(f2_, f3_, sqrtsVal, kILC, fa2_, fa3_);
-      h_var->Fill(fa3_);
-      h_var_pull->Fill(f3_pull_);
-    }
+    
   }
 
   h_var->SetXTitle(varName);
@@ -173,17 +236,62 @@ void drawsingle(float sqrtsVal, float f2gen, float f3gen, TString toyName, int v
   if ( variable == kFA3 ) 
     c1->SetRightMargin(0.11);
   h_var->Draw("hist");
-  h_var->Fit("gaus");
-  c1->SaveAs(Form("toyresults_ilc/plots/%s_fitresults_%s.eps", var.Data(), toyName.Data()));
-  c1->SaveAs(Form("toyresults_ilc/plots//%s_fitresults_%s.png", var.Data(), toyName.Data()));
+  double xfit_min = TMath::Max (h_var->GetMean() - 3*h_var->GetRMS(), 0.);
+  double xfit_max = TMath::Min (h_var->GetMean() + 3*h_var->GetRMS(), 1.);
+  h_var->Fit("gaus", "", "", xfit_min, xfit_max);
 
+  //
+  // get the fit results and calculate the fa3 errors
+  // 
+  TF1 *gfit = (TF1 *)h_var->GetFunction("gaus");
+  float fitmean = gfit->GetParameter(1); 
+  float fitsigma = gfit->GetParameter(2); 
+
+
+  float f3mean = fitmean; 
+  float f3sigma = fitsigma; 
+  std::cout << "-----------------------------------------------------\n";
+  std::cout << Form("Results for sqrts = %.0f GeV\n", sqrtsVal) ;
+  std::cout << Form("direct fit results : %.3f +/- %.3f\n", fitmean, fitsigma);
+  float f3_p1sigma = f3mean + f3sigma;
+  float f3_m1sigma = f3mean - f3sigma;  
+  
+  // special for the ppZH, need to convert the f3 after cut to before the cut
+  if ( collider == kLHCZH && variable == kF3ACC) {
+    f3mean = calcfacc(fitmean);
+    f3_p1sigma = calcfacc(fitmean + fitsigma);
+    f3_m1sigma = calcfacc(fitmean - fitsigma);
+  }
+  std::cout << Form("f3 (before cuts): %.3f (+ %.3f, -%.3f)\n",  f3mean, f3_p1sigma-f3mean, f3mean-f3_m1sigma);
+
+  float fa2_mean = 0.; // dummy  
+  float fa3_mean = 0.;
+  calcgcoup(0, f3mean, sqrtsVal, collider, fa2_mean , fa3_mean);
+
+  float fa3_p1sigma = 0.;
+  float fa2_p1sigma = 0.; // dummy
+  calcgcoup(0, f3_p1sigma, sqrtsVal, collider, fa2_p1sigma , fa3_p1sigma);
+
+  float fa3_m1sigma = 0.;
+  float fa2_m1sigma = 0.; // dummy
+  calcgcoup(0, f3_m1sigma, sqrtsVal, collider, fa2_m1sigma , fa3_m1sigma);
+
+  std::cout << Form("fa3 generator input : %.7f\n", fa3gen);   
+  std::cout << Form("fa3 results from fit : %.7f (+%.7f, -%.7f)\n", fa3_mean, fa3_p1sigma-fa3_mean, fa3_mean-fa3_m1sigma); 
+  std::cout << "-----------------------------------------------------\n";
+
+  c1->SaveAs(Form("%s/plots/%s_fitresults_%s.eps", toyDir.Data(), var.Data(), toyName.Data()));
+  c1->SaveAs(Form("%s/plots//%s_fitresults_%s.png", toyDir.Data(), var.Data(), toyName.Data()));
+
+  /*
   c1->Clear();
   h_var_pull->Draw("hist");
   h_var_pull->Fit("gaus");
   
-  c1->SaveAs(Form("toyresults_ilc/plots/%s_pull_fitresults_%s.eps", var.Data(), toyName.Data()));
-  c1->SaveAs(Form("toyresults_ilc/plots/%s_pull_fitresults_%s.png", var.Data(), toyName.Data()));
+  c1->SaveAs(Form("%s/plots/%s_pull_fitresults_%s.eps", toyDir.Data(), var.Data(), toyName.Data()));
+  c1->SaveAs(Form("%s/plots/%s_pull_fitresults_%s.png", toyDir.Data(), var.Data(), toyName.Data()));
 
+  */
   delete h_var;
   delete h_var_pull;
   delete c1;
@@ -326,7 +434,7 @@ void draw2dsingle(TString toyName, TString xVar, TString yVar, TString xTitle, T
 }
 
 
-void calcgcoup(double f2, double f3, double sqrts = 250., colliderType machine = kLHC, float &fa2, float &fa3){
+void calcgcoup(double f2, double f3, double sqrts = 250., colliderType machine = kLHCZH, float &fa2, float &fa3){
 
   Double_t sigma1;
   Double_t sigma2;
@@ -365,13 +473,24 @@ void calcgcoup(double f2, double f3, double sqrts = 250., colliderType machine =
     sigma4 = sigma4_e;
   }
 
-
-  // LHC numbers at mH= 125 GeV (JHUGen)
+  // For the decay
+  Double_t sigma1_pdecay = 1.860351; // was 2.03971 at 126 GeV
+  Double_t sigma2_pdecay = 0.672859; // was 0.77517 at 126 GeV
+  Double_t sigma4_pdecay = 0.284353; // was 0.32689 at 126 GeV
+  
   Double_t sigma1_p = 1.860351; // was 2.03971 at 126 GeV
   Double_t sigma2_p = 0.672859; // was 0.77517 at 126 GeV
   Double_t sigma4_p = 0.284353; // was 0.32689 at 126 GeV
 
-  if (machine == kLHC) {
+
+ if ( machine == kLHCZH ) {
+    sigma1_p = 198.7906;
+    sigma2_p = 0.0000000001;
+    // sigma4_p = 9493.1962;
+    sigma4_p = 9000.;
+  }
+
+  if (machine == kLHCZH || machine == kLHCDECAY) {
     sigma1 = sigma1_p;
     sigma2 = sigma2_p;
     sigma4 = sigma4_p;
@@ -385,17 +504,30 @@ void calcgcoup(double f2, double f3, double sqrts = 250., colliderType machine =
   double g2 = sqrt(f2 * sigma_total  / sigma2);
   double g4 = sqrt(f3 * sigma_total  / sigma4);
 
-  // std::cout << "--- f2 = " << f2 << ", f3 = " << f3 << "\n"; 
-  // std::cout << "--- g1 = 1, g2 = " << g2 << ", g4 = " << g4 << "\n";
-
   // calculate fa2 and fa3
   
-  Double_t fg2_p = sigma2_p*g2*g2 / (  sigma1_p*g1*g1 + sigma2_p*g2*g2 + sigma4_p*g4*g4 );
-  Double_t fg4_p = sigma4_p*g4*g4 / (  sigma1_p*g1*g1 + sigma2_p*g2*g2 + sigma4_p*g4*g4 );
+ // calculate fa2 and fa3
   
-  //   std::cout << " --- LHC (mH=125 GeV):   fa2 = " << fg2_p << "   fa3 = "<< fg4_p << endl;   
+  Double_t fg2_pdecay = sigma2_pdecay*g2*g2 / (  sigma1_pdecay*g1*g1 + sigma2_pdecay*g2*g2 + sigma4_pdecay*g4*g4 );
+  Double_t fg4_pdecay = sigma4_pdecay*g4*g4 / (  sigma1_pdecay*g1*g1 + sigma2_pdecay*g2*g2 + sigma4_pdecay*g4*g4 );
+
+  /*
+  std::cout << "--- f2 = " << f2 << ", f3 = " << f3 << "\n"; 
+  std::cout << "--- g1 = 1, g2 = " << g2 << ", g4 = " << g4 << "\n";
+  std::cout << " --- LHC (mH=125 GeV):   fa2 = " << fg2_pdecay << "   fa3 = "<< fg4_pdecay << endl; 
+  */  
+
+  fa2 = fg2_pdecay;
+  fa3 = fg4_pdecay;
   
-  fa2 = fg2_p;
-  fa3 = fg4_p;
+}
+
+double calcfacc(double f3_acc)
+{
+  // R = eff_0plus_vs_0minus 
+  double R =  0.176893;
+  //   double f3 = (R*f3_acc) / (1+(R-1)*f3_acc);
+  double f3 = 1. / (1. + (1./f3_acc-1.)/R);
+  return f3;
   
 }
